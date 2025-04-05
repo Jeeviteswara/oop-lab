16e4037db1463d87215d75ab90812ef4b54aab52
Jeeviteswara <reddyjeeviteswara@gmail.com>
2025-04-06 00:30:01 +0530

    Delete Main777.java

--- Main777.java
+++
@@ -1,8 +0,0 @@
-import maths.Calculator;   
-public class Main777 {   
-public static void main(String[] args) {   
-Calculator calc = new Calculator();   
-System.out.println("Addition: " + calc.add(10, 5));   
-System.out.println("Subtraction: " + calc.subtract(10, 5));   
-}   
-}  
