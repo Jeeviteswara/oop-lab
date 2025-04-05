cd5df73273df2dd85b414c9db711fac82b3ce54e
Jeeviteswara <reddyjeeviteswara@gmail.com>
2025-04-06 00:38:43 +0530

    Add files via upload

---
+++ Packages/Main15d.java
@@ -0,0 +1,8 @@
+import java.util.Random;   
+public class Main15d {   
+public static void main(String[] args) {   
+Random rand = new Random();   
+int randomNum = rand.nextInt(1000); 
+System.out.println("Random Number: " + randomNum);   
+}   
+}
