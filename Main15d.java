54c4e214742a2142a4b40e0f134fe6ac814b59e9
Jeeviteswara <reddyjeeviteswara@gmail.com>
2025-04-06 00:29:41 +0530

    Delete Main15d.java

--- Main15d.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;   
-public class Main15d {   
-public static void main(String[] args) {   
-Random rand = new Random();   
-int randomNum = rand.nextInt(1000); 
-System.out.println("Random Number: " + randomNum);   
-}   
-}
