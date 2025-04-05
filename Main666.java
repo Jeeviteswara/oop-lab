2f4b4212fea364c865203f8ebcbb19e70d88eb43
Jeeviteswara <reddyjeeviteswara@gmail.com>
2025-04-06 00:27:17 +0530

    Add files via upload

---
+++ Main666.java
@@ -0,0 +1,7 @@
+import mypackage.Greeting;   
+public class Main666 {   
+public static void main(String[] args) {   
+Greeting greet = new Greeting();   
+greet.sayHello();   
+}   
+}  
---
+++ Main777.java
@@ -0,0 +1,8 @@
+import maths.Calculator;   
+public class Main777 {   
+public static void main(String[] args) {   
+Calculator calc = new Calculator();   
+System.out.println("Addition: " + calc.add(10, 5));   
+System.out.println("Subtraction: " + calc.subtract(10, 5));   
+}   
+}  
